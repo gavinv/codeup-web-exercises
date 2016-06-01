fff7188dbc4790c998ef714c05e737da9caf0687
Gavin <gavinvaught@gmail.com>
2016-06-01 15:59:17 -0500

    Added month switch code

--- public/js/switch.js
+++ public/js/switch.js
@@ -1,4 +1,4 @@
-var luckyNumber = Math.floor(Math.random()* 6)
+var luckyNumber = Math.floor(Math.random()* 6);
 
 	switch(luckyNumber) {
 		case 0:
@@ -22,4 +22,47 @@ var luckyNumber = Math.floor(Math.random()* 6)
 		default:
 			console.log("I'm broken.");
 
+	}
+
+var month = Math.floor(Math.random()* 12 + 1);
+
+	switch(month) {
+		case 1:
+			console.log("January");
+			break;
+		case 2:
+			console.log("February");
+			break;
+		case 3:
+			console.log("March");
+			break;
+		case 4:
+			console.log("April");
+			break;
+		case 5:
+			console.log("May");
+			break;
+		case 6:
+			console.log("June");
+			break;
+		case 7:
+			console.log("July");
+			break;
+		case 8:
+			console.log("August");
+			break;
+		case 9:
+			console.log("September");
+			break;
+		case 10:
+			console.log("October");
+			break;
+		case 11:
+			console.log("November");
+			break;
+		case 12:
+			console.log("December");
+			break;
+		default:
+			console.log("I'm broken :-(")
 	}
