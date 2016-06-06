10632498f425980cabb6ca5b24071a82334313e9
Gavin <gavinvaught@gmail.com>
2016-06-06 14:01:31 -0500

    creates person object and properties. line 16 calls an alert using properties.

--- public/js/objects.js
+++ public/js/objects.js
@@ -4,9 +4,16 @@
     // TODO: Create person object
     // var person = todo;
 
+var person = {}
+
+person.firstName = 'Gavin';
+person.lastName = 'Vaught';
+
     // TODO: Create firstName and lastName properties in your person object; assign your name to them
 
     // TODO: Add a sayHello method to the person object that
+
+var sayHello = alert("Hello " + person.firstName + " " + person.lastName + "!");
     //       alerts a greeting using the firstName and lastName properties
 
     // Say hello from the person object.
