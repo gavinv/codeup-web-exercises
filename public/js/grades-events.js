158ee2ef3677f9a09a83334ed98856b6369a5afb
Gavin <gavinvaught@gmail.com>
2016-06-09 16:34:01 -0500

    added grade average event exercise

---
+++ public/js/grades-events.js
@@ -0,0 +1,71 @@
+// name entry variables
+var myName = document.getElementById('name');
+var saveName = document.getElementById('save-name');
+var studentName = document.getElementById('student-name');
+
+// grade entry variables
+var subjectEntry = document.getElementById('subject');
+var gradeEntry = document.getElementById('grade');
+var addGrade = document.getElementById('add-grade');
+var calculateBtn = document.getElementById('calculate-average');
+
+// table variables
+var table = document.getElementById('grades');
+var stuAvg = document.getElementById('student-average');
+
+// is awesome
+var awesome = document.getElementById('student-awesome');
+var practice = document.getElementById('student-practice');
+
+// Submits name and removes disabled grade buttons
+var submitName = function() {
+	studentName.innerHTML = myName.value;
+	addGrade.removeAttribute('disabled');
+	calculateBtn.removeAttribute('disabled');
+};
+saveName.addEventListener('click', submitName, false);
+
+// add info to table
+var newGrade = function() {
+var newRow = document.createElement('tr');
+var newContent = "<td>"+ subjectEntry.value +"</td>" + "<td>"+ gradeEntry.value +"</td>";
+newRow.innerHTML = newContent;
+table.insertBefore(newRow, table.firstChild);
+student.addSubject(subjectEntry.value, Number(gradeEntry.value));
+};
+addGrade.addEventListener('click', newGrade, false);
+
+// math!
+var student = {
+    awesomeGrade: 80,
+    name: null,
+    subjects: [],
+    calculateAverage: function () {
+        var average = 0;
+        this.subjects.forEach(function (subject) {
+            average += subject.grade;
+        });
+        return average / this.subjects.length;
+    },
+    addSubject: function (name, grade) {
+        var subject = {
+            name: name,
+            grade: grade
+        };
+        console.log(subject);
+        this.subjects.push(subject);
+    },
+    isAwesome: function () {
+        return this.calculateAverage() > this.awesomeGrade;
+    }
+};
+console.log(student.isAwesome());
+var calcAvg = function() {
+	stuAvg.innerHTML = Math.round(student.calculateAverage());
+	if (student.isAwesome()) {
+		awesome.classList.remove('hidden');
+	} else {
+		practice.classList.remove('hidden');
+	};
+};
+calculateBtn.addEventListener('click', calcAvg, false);
