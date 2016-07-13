72b331672fe487bbda0c19edc1f13eccfcb81eb6
Gavin <gavinvaught@gmail.com>
2016-07-13 15:48:27 -0500

    created a page with php to print random ajectives and nouns to page

---
+++ public/server-name-generator.php
@@ -0,0 +1,15 @@
+<?php
+	$adjectives = ['Buffy', 'Spicy', 'Soft', 'Misinformed', 'Great', 'Big', 'Salty', 'Hungry', 'Aloof', 'Hairy', 'Frustrated', 'Calm', 'Smoky', 'Wonderful', 'Incandescent', 'Tossed', 'Quirky', 'Opaque', 'Patriotic', 'Decapitated', 'Elegant', 'Caffinated', 'Electrocuted', 'Dank', 'Frisky'];
+	$nouns = ['Ocean', 'Developer', 'Cups', 'Brain', 'Devil', 'Elvis', 'Fruit', 'Rack', 'Space', 'Server', 'Name', 'Person', 'Book', 'Desk', 'Coffee', 'Duck', 'Doggo', 'Elephant', 'Lunch', 'Pupper', 'Pikachu', 'Meme', 'Salad', 'Laser', 'Dingo', 'Tip', 'John', 'Shaft', 'Job'];
+	$randomAdj = array_rand($adjectives, 1);
+	$randomNoun = array_rand($nouns, 1);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Server Name Generator</title>
+</head>
+<body>
+<h1><?php echo $adjectives[$randomAdj] . ' ' . $nouns[$randomNoun]; ?></h1>
+</body>
+</html>
