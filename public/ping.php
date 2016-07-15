b0110a2c58736e02a399f036381adbedddc6f6cb
Gavin <gavinvaught@gmail.com>
2016-07-15 11:49:19 -0500

    used counter function from conter.php file to create a ping pong game that takes requests from the other page to increase score.

---
+++ public/ping.php
@@ -0,0 +1,27 @@
+<?php
+	function pageController() 
+	{
+		if (isset($_GET['count'])){
+			$count = $_GET['count'];
+		} else {
+			$count = 0;
+		}
+		if (isset($_GET['data'])) {
+			$count = 'GAME OVER PONG';
+		}
+		return ['count' => $count];
+	}
+	extract(pageController());
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PING...</title>
+</head>
+<body>
+	<h3>PING</h3>
+	<h3>Score: <?= $count ?></h3>
+	<a href="/pong.php?count=<?= $count+1 ?>">HIT</a> //
+	<a href="/pong.php?data=miss">MISS</a>
+</body>
+</html>
---
+++ public/pong.php
@@ -0,0 +1,27 @@
+<?php
+	function pageController() 
+	{
+		if (isset($_GET['count'])){
+			$count = $_GET['count'];
+		} else {
+			$count = 0;
+		}
+		if (isset($_GET['data'])) {
+			$count = 'GAME OVER PING';
+		}
+		return ['count' => $count];
+	}
+	extract(pageController());
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PONG...</title>
+</head>
+<body>
+	<h3>PONG</h3>
+	<h3>Score: <?= $count ?></h3>
+	<a href="/ping.php?count=<?= $count+1 ?>">HIT</a> //
+	<a href="/ping.php?data=miss">MISS</a>
+</body>
+</html>
