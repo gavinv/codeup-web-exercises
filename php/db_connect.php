17b7c7da2a9fc0d88cab6f242efb5c397821811a
Gavin <gavinvaught@gmail.com>
2016-08-02 10:57:16 -0500

    defined constants for info to connect to db using php

--- php/db_connect.php
+++ php/db_connect.php
@@ -1,6 +1,15 @@
 <?php
+// IP address to connect to
+define('DB_HOST', '127.0.0.1');
+// The database to connect to
+define('DB_NAME', 'employees');
+// The mySQL user to use
+define('DB_USER', 'vagrant');
+// Password for MySQL user
+define('DB_PASS', 'vagrant');
+
 // Get new instance of PDO object
-$dbc = new PDO('mysql:host=127.0.0.1;dbname=employees', 'codeup_test_user', 'bP=45q');
+$dbc = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
 
 // Tell PDO to throw exceptions on error
 $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
