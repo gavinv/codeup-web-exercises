13e144c08b53d9d3750253934e704135db55d9cd
Gavin <gavinvaught@gmail.com>
2016-08-09 14:15:05 -0500

    believing

--- User.php
+++ User.php
@@ -6,58 +6,66 @@ require_once __DIR__ . '/Model.php';
 
 class User extends Model
 {
-    /** Insert a new entry into the database */
-    protected function insert()
-    {
-        // @TODO: Use prepared statements to ensure data security
+	/** Insert a new entry into the database */
+	protected function insert()
+	{
+		$stmt = $dbc->prepare('INSERT INTO users (username, password, email) VALUES (:username, :password, :email)');
 
-        // @TODO: You will need to iterate through all the attributes to build the prepared query
+		$stmt->bindValue(':username', $this->attributes['username'], PDO::PARAM_STR);
+		$stmt->bindValue(':password',  $this->attributes['password'],  PDO::PARAM_STR);
+		$stmt->bindValue(':email', $this->attributes['email'], PDO::PARAM_STR);
 
-        // @TODO: After the insert, add the id back to the attributes array
-        //        so the object properly represents a DB record
-    }
+		$stmt->execute();
 
-    /** Update existing entry in the database */
-    protected function update()
-    {
-        // @TODO: Use prepared statements to ensure data security
+		$this->attributes['id'] = self::$dbc->lastInsertId();
+	}
 
-        // @TODO: You will need to iterate through all the attributes to build the prepared query
-    }
+	/** Update existing entry in the database */
+	protected function update()
+	{
+		$stmt = $dbc->prepare('UPDATE users SET username = :username, password = :password, email = :email WHERE id = :id');
 
-    /**
-     * Find a single record in the DB based on its id
-     *
-     * @param int $id id of the user entry in the database
-     *
-     * @return User An instance of the User class with attributes array set to values from the database
-     */
-    public static function find($id)
-    {
-        // Get connection to the database
-        self::dbConnect();
+		$stmt->bindValue(':username', $this->attributes['username'], PDO::PARAM_STR);
+		$stmt->bindValue(':password',  $this->attributes['password'],  PDO::PARAM_STR);
+		$stmt->bindValue(':email', $this->attributes['email'], PDO::PARAM_STR);
+		$stmt->bindValue(':id', $this->attributes['id'], PDO::PARAM_STR);
 
-        // @TODO: Create select statement using prepared statements
+		$stmt->execute();
+	}
 
-        // @TODO: Store the result in a variable named $result
+		/**
+		 * Find a single record in the DB based on its id
+		 *
+		 * @param int $id id of the user entry in the database
+		 *
+		 * @return User An instance of the User class with attributes array set to values from the database
+		 */
+		public static function find($id)
+		{
+			// Get connection to the database
+			self::dbConnect();
+			
+				// @TODO: Create select statement using prepared statements
 
-        // The following code will set the attributes on the calling object based on the result variable's contents
-        $instance = null;
-        if ($result) {
-            $instance = new static($result);
-        }
-        return $instance;
-    }
+				// @TODO: Store the result in a variable named $result
 
-    /**
-     * Find all records in a table
-     *
-     * @return User[] Array of instances of the User class with attributes set to values from database
-     */
-    public static function all()
-    {
-        self::dbConnect();
+				// The following code will set the attributes on the calling object based on the result variable's contents
+			$instance = null;
+			if ($result) {
+				$instance = new static($result);
+			}
+			return $instance;
+		}
 
-        // @TODO: Learning from the find method, return all the matching records
-    }
-}
+		/**
+		 * Find all records in a table
+		 *
+		 * @return User[] Array of instances of the User class with attributes set to values from database
+		 */
+		public static function all()
+		{
+			self::dbConnect();
+
+				// @TODO: Learning from the find method, return all the matching records
+		}
+	}
