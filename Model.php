c2972f1e5c5091edc52e99bb80191b2ddfaa992b
Gavin <gavinvaught@gmail.com>
2016-08-09 11:36:10 -0500

    finished building crud methods for model class

--- Model.php
+++ Model.php
@@ -2,88 +2,95 @@
 
 abstract class Model
 {
-    /** @var PDO|null Connection to the database */
-    protected static $dbc = null;
+	/** @var PDO|null Connection to the database */
+	protected static $dbc = null;
 
-    /** @var array Database values for a single record. Array keys should be column names in the DB */
-    protected $attributes = array();
+	/** @var array Database values for a single record. Array keys should be column names in the DB */
+	protected $attributes = array();
 
-    /**
-     * Constructor
-     *
-     * An instance of a class derived from Model represents a single record in the database.
-     *
-     * $param array $attributes Optional array of database values to initialize the model record with
-     */
-    public function __construct(array $attributes = array())
-    {
-        self::dbConnect();
+		/**
+		 * Constructor
+		 *
+		 * An instance of a class derived from Model represents a single record in the database.
+		 *
+		 * $param array $attributes Optional array of database values to initialize the model record with
+		 */
+		public function __construct(array $attributes = array())
+		{
+			self::dbConnect();
+			$this->attributes = $attributes;
+		}
 
-        // @TODO: Initialize the $attributes property with the passed value
-    }
+		/**
+		 * Connect to the DB
+		 *
+		 * This method should be called at the beginning of any function that needs to communicate with the database
+		 */
 
-    /**
-     * Connect to the DB
-     *
-     * This method should be called at the beginning of any function that needs to communicate with the database
-     */
+		// In order to use dbConnect, PDO object reqyures db config
+		// file to define DB_HOST, DB_NAME, DB_USER, and DB_PASS
+		// define('DB_HOST', '127.0.0.1');
+		// define('DB_NAME', 'db_name');
+		// define('DB_USER', 'db_username');
+		// define('DB_PASS', 'db_password');
+		protected static function dbConnect()
+		{
+			if (!self::$dbc) {
+				$dbc = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
+				$dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			}
+		}
 
-    // In order to use dbConnect, PDO object reqyures db config
-    // file to define DB_HOST, DB_NAME, DB_USER, and DB_PASS
-    // define('DB_HOST', '127.0.0.1');
-    // define('DB_NAME', 'db_name');
-    // define('DB_USER', 'db_username');
-    // define('DB_PASS', 'db_password');
-    protected static function dbConnect()
-    {
-        if (!self::$dbc) {
-            $dbc = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
-            $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        }
-    }
+		/**
+		 * Get a value from attributes based on its name
+		 *
+		 * @param string $key key for attributes array
+		 *
+		 * @return mixed|null value from the attributes array or null if it is undefined
+		 */
+		public function __get($key, $default = null)
+		{
+			if (array_key_exists($key, $this->attributes)) {
+				return $this->attributes[$key];
+			} else {
+				return $default;
+			}
+		}
 
-    /**
-     * Get a value from attributes based on its name
-     *
-     * @param string $name key for attributes array
-     *
-     * @return mixed|null value from the attributes array or null if it is undefined
-     */
-    public function __get($name)
-    {
-        // @TODO: Return the value from attributes for $name if it exists, else return null
-    }
+		/**
+		 * Set a new value for a key in attributes
+		 *
+		 * @param string $key  key for attributes array
+		 * @param mixed  $value value to be saved in attributes array
+		 */
+		public function __set($key, $value)
+		{
+			$this->attributes[$key] = $value;
+		}
 
-    /**
-     * Set a new value for a key in attributes
-     *
-     * @param string $name  key for attributes array
-     * @param mixed  $value value to be saved in attributes array
-     */
-    public function __set($name, $value)
-    {
-        // @TODO: Store name/value pair in attributes array
-    }
+		/** Store the object in the database */
+		public function save()
+		{
+			if(!empty($this->attributes)) {
+				if(isset($this->attributes['id'])) {
+					$this->update();
+				} else {
+					$this->insert();
+				}
+			}
+		}
 
-    /** Store the object in the database */
-    public function save()
-    {
-        // @TODO: Ensure there are values in the attributes array before attempting to save
+		/**
+		 * Insert new entry into database
+		 *
+		 * NOTE: Because this method is abstract, any child class MUST have it defined.
+		 */
+		protected abstract function insert();
 
-        // @TODO: Call the proper database method: if the `id` is set this is an update, else it is a insert
-    }
-
-    /**
-     * Insert new entry into database
-     *
-     * NOTE: Because this method is abstract, any child class MUST have it defined.
-     */
-    protected abstract function insert();
-
-    /**
-     * Update existing entry in database
-     *
-     * NOTE: Because this method is abstract, any child class MUST have it defined.
-     */
-    protected abstract function update();
-}
+		/**
+		 * Update existing entry in database
+		 *
+		 * NOTE: Because this method is abstract, any child class MUST have it defined.
+		 */
+		protected abstract function update();
+	}
